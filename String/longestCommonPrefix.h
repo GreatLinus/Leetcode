76b31226dc3fe93b31dee5aafde8d74854669f11
Han <hanyujie2269@163.com>
2021-04-11 13:41:00 +0800

    Initial commit

---
+++ String/longestCommonPrefix.h
@@ -0,0 +1,12 @@
+//
+// Created by Han on 2021/4/8.
+//
+
+#ifndef LEETCODE_LONGESTCOMMONPREFIX_H
+#define LEETCODE_LONGESTCOMMONPREFIX_H
+
+#endif //LEETCODE_LONGESTCOMMONPREFIX_H
+
+string longestCommonPrefix(string &str){
+
+}
